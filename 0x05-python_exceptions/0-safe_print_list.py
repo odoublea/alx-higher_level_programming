23ad7cd0fb551ee2890e33fdc30b14eecdca5dbf
odoublea <oladegakenny@gmail.com>
2022-06-20 16:55:40 -0700

    safe list printing

---
+++ 0x05-python_exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/bin/usr/python
+def safe_print_list(my_list=[], x=0):
+    elements = 0
+    for index in range(x):
+        try:
+            print(my_list[index], end='')
+            elements += 1
+        except Exception as error:
+            break
+    print('')
+    return elements
+
