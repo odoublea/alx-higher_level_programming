f729d2a52e371a893b4e4018313896479c3e5f03
Abdulqudus Oladega <oladegakenny@gmail.com>
2022-06-22 04:56:05 +0000

    access and update private attribute

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -21,15 +21,4 @@ class Square:
         self.__size = size
 
     def area(self):
-        return (self.__size ** 2)lass Square:
-    '''A square has four equal sides. Calulate the area of a square*2'''
-
-    def __init__(self, size=0):
-        if type(size) != int:
-            raise TypeError('size must be an integer')
-        elif size < 0:
-            raise ValueError('size must be >= 0')
-        self.__size = size
-
-    def area(self):
         return (self.__size ** 2)
