a6db5355102507fea229439d3f374a2903489056
Oladega Abdulqudus <oladegakenny@gmail.com>
2022-06-22 03:50:21 +0100

    object oriented programming

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''This module defines a square class'''
+
+class Square:
+'''This class square constructs a square
+
+It contains no method
+'''
+	pass
+	''' This ensures our class is skipped'''
+
