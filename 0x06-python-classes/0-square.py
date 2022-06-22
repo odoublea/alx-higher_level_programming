49762b5b76eb31ae66ef62a914806ac9c10117e9
Oladega Abdulqudus <oladegakenny@gmail.com>
2022-06-22 03:55:35 +0100

    my first square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,10 +2,7 @@
 '''This module defines a square class'''
 
 class Square:
-'''This class square constructs a square
-
-It contains no method
-'''
+	'''This class square constructs a square
+	It contains no method
+	'''
 	pass
-	''' This ensures our class is skipped'''
-
