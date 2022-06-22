4b7d1325541d2104bf19c83b09cbade670edb8e4
Abdulqudus Oladega <oladegakenny@gmail.com>
2022-06-22 04:13:22 +0000

    square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''This module contains a Square class that has a private instance'''
+
+
+class Square:
+    '''A square has four equal sides.'''
+
+    def __init__(self, size):
+        self.size = size
