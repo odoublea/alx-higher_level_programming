cce672f1011ec8e129a524c3c079f8615afc9850
Oladega Abdulqudus <oladegakenny@gmail.com>
2022-06-27 05:54:14 +0100

    unit testing

---
+++ 0x06-python-classes/pycodestyletest.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''This module defines a square and calculates its area'''
+
+
+class Square:
+    """Square implementation. All squares have an area"""
+
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if type(size) != int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    def area(self):
+        return (self.__size ** 2)
