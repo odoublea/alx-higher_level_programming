f18002b6f900c1e8090ba03f8547e566dd0f6bc4
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-01-05 06:20:22 -0800

    Code snipe

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,18 +2,14 @@
 if __name__ == "__main__":
     from sys import argv
 
-    len = len(argv)
-    arg_num = len - 1
+    arg_num = len(argv) - 1
     if arg_num == 0:
         print("{} argument.".format(arg_num))
     elif arg_num == 1:
         print("{} arguments:".format(arg_num))
-        print("{}: {}".format(arg_num, argv[arg_num]))
-
+        print("{}: {}".format(arg_num, argv[1]))
     else:
         print("{} arguments:".format(arg_num))
-        for i in argv:
+        for i in argv[1:]:
             arg = argv.index(i)
-            if arg == 0:
-                continue
             print("{}: {}".format(arg, i))
