53c4acd2658a169dab2fd89c852b66229b3c3f2b
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-01-05 13:08:33 -0800

    magic calculator

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    def magic_calculation(a, b):
-        from calculator_1 import add, sub
-        if a < b:
-            c = add(a, b)
-        c = 0
+def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub
+    if a < b:
+        c = add(a, b)
+  
         for i in range(90):
-            c = add(c, i)
+             c = add(c, i)
+        print(c)
+    sub(a, b)
