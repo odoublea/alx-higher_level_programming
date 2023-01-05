bf652f66bce2fa560ffd3663cbe7e13ad83f5982
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-01-05 13:48:46 -0800

    arghhs again

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,12 +5,11 @@ if __name__ == "__main__":
     number_of_args = len(argv) - 1
 
     if number_of_args == 0:
-        print("{} argument.".format(number_of_args))
-    elif number_of_args == 1:
-        print("{} arguments:".format(number_of_args))
-        print("{}: {}".format(number_of_args, argv[1]))
-    else:
-        print("{} arguments:".format(number_of_args))
+        print("0 argument.")
+    if number_of_args >= 1:
+        print("{} argument".format(number_of_args) if number_of_args < 2 else\
+               "{} arguments".format(number_of_args, argv[1]))
+        
         for i in argv[1:]:
             arg = argv.index(i)
             print("{}: {}".format(arg, i))
