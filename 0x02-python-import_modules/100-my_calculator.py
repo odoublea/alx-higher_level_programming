a855bbdd7701fb94468fc3d4d97cc24dcb09bfbf
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-01-05 15:41:14 -0800

    args and multiply

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,29 +1,27 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv, exit
+    from sys import argv
     from calculator_1 import *
 
     num_of_args = len(argv)
     
-    if num_of_args < 4:
+    if num_of_args != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
-    a = int(argv[1])
-    b = int(argv[3])
-    operator = argv[2]
-    function = [add(a, b), sub(a, b), mul(a, b), div(a, b)]
+    a, operator, b = int(argv[1]), argv[2], int(argv[3])
     operators = ["+", "-", "*", "/"]
-
+    result = 0
 
     if operator not in operators:
         print("Unknown operator. Available operators: +, -, *, and /")
         exit(1)
     elif operator == "+":
-        print("{} {} {} = {}".format(a, operator, b, function[0]))
+        result = add(a, b)
     elif operator == "-":
-        print("{} {} {} = {}".format(a, operator, b, function[1]))
+        result = sub(a, b)
     elif operator == "*":
-        print("{} {} {} = {}".format(a, operator, b, function[2]))
+        result = mul(a, b)
     else:
-        print("{} {} {} = {}".format(a, operator, b, function[3]))
+        result = div(a, b)
+    print("{} {} {} = {}".format(a, operator, b, result))
