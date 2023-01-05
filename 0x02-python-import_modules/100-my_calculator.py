8cd9989a153cfe6afd399dfe6dcc45762d0f9eb4
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-01-05 15:16:21 -0800

    argsss

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,22 +2,28 @@
 if __name__ == "__main__":
     from sys import argv, exit
     from calculator_1 import *
-   
-    num_of_args = len(argv) - 1
 
-    if num_of_args != 4:
+    num_of_args = len(argv)
+    
+    if num_of_args < 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
+
     a = int(argv[1])
     b = int(argv[3])
-    
+    operator = argv[2]
+    function = [add(a, b), sub(a, b), mul(a, b), div(a, b)]
     operators = ["+", "-", "*", "/"]
-    functions = [add(a, b), sub(a, b), mul(a, b), div(a, b)]
-    if not(argv[2] in operators):
+
+
+    if operator not in operators:
         print("Unknown operator. Available operators: +, -, *, and /")
         exit(1)
-
-    for operator, function in zip(operators, functions):
-        if operator == argv[2]:
-            break
-    print("{} {} {} = {}".format(a, argv[2], b, function))
+    elif operator == "+":
+        print("{} {} {} = {}".format(a, operator, b, function[0]))
+    elif operator == "-":
+        print("{} {} {} = {}".format(a, operator, b, function[1]))
+    elif operator == "*":
+        print("{} {} {} = {}".format(a, operator, b, function[2]))
+    else:
+        print("{} {} {} = {}".format(a, operator, b, function[3]))
