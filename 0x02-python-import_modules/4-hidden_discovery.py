2b2e5b9b507836df8fcdff7b8a09c733cc93116c
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-01-05 07:27:17 -0800

    who are you?

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+    hidden = dir(hidden_4)
+
+    for name in hidden:
+        if not name.startswith("__"):
+            print(name)
