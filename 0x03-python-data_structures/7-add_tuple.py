fca3003bfeb39300cbf652f3840bf225bb5f0168
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-01-07 14:59:11 -0800

    add tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,5 +2,5 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     tuple_a += (0, 0)
     tuple_b += (0, 0)
-    tuple_c = tuple(map(lambda i, j : i + j, tuple_a, tuple_b))
+    tuple_c = tuple(map(lambda i, j: i + j, tuple_a, tuple_b))
     return tuple_c[:2]
