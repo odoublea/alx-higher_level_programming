242a445fd9af3d1fb424866b9a545e138dd1feb6
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-01-07 13:38:34 -0800

    Can you see C me now

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    my_string = my_string.split()
-    for ch in my_string:
-        if ch == 'c' or ch == 'C':
-            del ch
-
-        my_string = " ".join(ch)
-    return my_string
+    return my_string.translate({ord('c'): None, ord('C'): None})
