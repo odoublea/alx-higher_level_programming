4530f626a190e5af62dc9bc5dd2c124df5d86cfb
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-01-08 14:55:12 -0800

    my_tuple

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence == '':
+    my_tuple = ()
+    length = len(sentence)
+    first = sentence[0]
+
+    if len(sentence) == 0:
+        my_tuple = length, 'None'
+
+    my_tuple = length, first
+    return my_tuple
