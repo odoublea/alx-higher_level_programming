475a82df1e4d5a1ce7a91ed28e00d122a322564e
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-01-08 14:58:38 -0800

    more returns\!

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,8 +4,8 @@ def multiple_returns(sentence):
     length = len(sentence)
     first = sentence[0]
 
-    if len(sentence) == 0:
+    if length == 0:
         my_tuple = 0, 'None'
-
     my_tuple = length, first
+
     return my_tuple
