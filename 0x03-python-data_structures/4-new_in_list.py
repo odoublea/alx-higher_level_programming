0172d2eb2c23fb7327ac2d44899ff69b4aa7b882
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-01-09 09:28:44 -0800

    if not list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if isinstance(my_list, list):
-        new_list = my_list.copy()
+    if not my_list:
+        return
+    new_list = my_list.copy()
     if 0 < idx < len(my_list):
         new_list[idx] = element
         return(new_list)
