88cb177160263c83f1f80cae2ad58f93baf5b4a8
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-01-09 09:35:02 -0800

    multiple return

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    my_tuple = ()
     length = len(sentence)
-    first = sentence[0]
-    if length == 0:
-        my_tuple = 0, 'None'
-    my_tuple = length, first
-    return(my_tuple)
+    first = sentence[0] if length > 0 else None
+    return(length, first)
