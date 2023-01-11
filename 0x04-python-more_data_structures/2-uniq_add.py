d0d3119eba5635613e26bea7fffd812bb05c7d6e
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-01-11 08:23:09 -0800

    removed the hashes

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     my_list = set(my_list)
-    #my_list = list(my_list)
-    #result = 0
-    #for i in range(len(my_list)):
-    #    result += my_list[i]
     return sum(my_list)
