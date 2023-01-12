8a395c03705aba15fd1b0fcff4f8fc64fedd043a
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-01-11 20:18:24 -0800

    Roman numerals

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
                       'C': 100, 'D': 500, 'M': 1000}
-    if not isinstance(roman_string, str) or roman_string == None:
+    if not isinstance(roman_string, str):
         return None
     num_sum = 0
     for i in range(len(roman_string)-1, -1, -1):
