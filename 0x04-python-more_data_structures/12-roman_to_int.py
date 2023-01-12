2bf6023903f393aa81a15fd734f355abb7b58478
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-01-11 20:08:21 -0800

    Roman to integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                      'C': 100, 'D': 500, 'M': 1000}
+    if not isinstance(roman_string, str):
+        return None
+    num_sum = 0
+    for i in range(len(roman_string)-1, -1, -1):
+        num = roman_numerals[roman_string[i]]
+        if 3*num < num_sum:
+            num_sum -= num
+        else:
+            num_sum += num
+    return num_sum
