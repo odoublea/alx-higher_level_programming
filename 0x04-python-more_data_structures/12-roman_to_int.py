19aa93abbfc70b3248c092c3a0a5f83957123dd5
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-01-11 20:34:41 -0800

    return 0 and always prefer the easiest way

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,11 +2,12 @@
 def roman_to_int(roman_string):
     roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     if not isinstance(roman_string, str):
-        return None
+        return 0
     num_sum = 0
-    for i in range(len(roman_string)):
-        if i > 0 and roman[roman_string[i]] > roman[roman_string[i - 1]]:
-            num_sum += roman[roman_string[i]] - 2 * roman[roman_string[i - 1]]
+    for i in range(len(roman_string)-1, -1, -1):
+        num = roman[roman_string[i]]
+        if (3 * num) < num_sum:
+            num_sum -= num
         else:
-            num_sum += roman[roman_string[i]]
+            num_sum += num
     return num_sum
