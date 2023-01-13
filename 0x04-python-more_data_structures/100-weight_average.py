ff99dabd2973037ed549fba0b89ecebe6a9d5fe5
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-01-13 10:49:34 -0800

    Squared by using map

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if not isinstance(my_list, list) or len(my_list) < 0:
+    if not isinstance(my_list, list) or len(my_list) == 0:
         return 0
     freq = sum((x * y) for x, y in my_list)
     weight = sum(j for i, j in my_list)
-    result = freq / weight
-    return result
+    weight_average = freq / weight
+    return weight_average
