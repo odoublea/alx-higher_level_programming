3482110553c3179ef6873a9b924bd8f599b112ee
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-01-13 09:54:31 -0800

    list comprehenion

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+from functools import reduce
 def weight_average(my_list=[]):
     if len(my_list) < 0:
         return 0
-    nominator = sum(map(sum, my_list))
+    nominator = sum((x * y) for x, y in my_list)
     denominator = sum(j for i, j in my_list)
     result = nominator / denominator
     return result
