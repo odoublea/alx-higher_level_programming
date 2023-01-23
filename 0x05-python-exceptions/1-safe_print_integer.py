78a0a155f530afeb86b25695206ad7e31887b142
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-01-23 17:57:42 +0000

    switch

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,7 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except ValueError:
-        return False
-    else:
+    except Exception:
         return True
+    else:
+        return False
