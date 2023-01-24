ab68586dbb26d327a83e2ee66f5fccd4ba46a55d
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-01-24 22:12:44 +0000

    Access and update private attribute

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -23,7 +23,7 @@ class Square:
 
         """
         self.__size = size
-        if not type(size) is int:
+        if not isinstance(size, int):
             raise TypeError('size must be an integer')
         if size < 0:
             raise ValueError('size must be >= 0')
--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -27,3 +27,19 @@ class Square:
             raise TypeError('size must be an integer')
         if size < 0:
             raise ValueError('size must be >= 0')
+
+    def area(self):
+        """Returns the area of the square."""
+        return (self.__size ** 2)
+
+    @property
+    def size(self):
+        """Get the value of size."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """set the value of size."""
+        self.__size = value
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
