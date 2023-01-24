68e3502956f30d1808570a2a324a3fa958d6c560
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-01-24 17:25:19 +0000

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+'''This script contain a class Square.'''
+
+
+class Square:
+    '''This is a class with a private size attribute.'''
+    def __init__(self, size):
+        self.__size = size
