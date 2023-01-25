9fa56527daee96d28fdbdc95f30fa2d52b21c890
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-01-25 04:14:27 +0000

    coordinates another style

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -59,11 +59,16 @@ class Square:
             TypeError: position must be a tuple of 2 positive integers.
 
         """
-        if not isinstance(value, tuple) or len(value) != 2 or \
-                not all(isinstance(num, int) for num in value) or \
-                not all(num >= 0 for num in value):
-            raise TypeError('position must be a tuple of 2 positive integers')
-        self.__position = value
+        if ((not isinstance(value[0], int)) or
+                (not isinstance(value[1], int))):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif (not isinstance(value, tuple) or
+                (value[0] < 0) or (value[1] < 0)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
 
     def my_print(self):
         """Print square hashes."""
