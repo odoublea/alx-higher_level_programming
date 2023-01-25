29ae364dba5943f60d1c556517aa1e96417f7f86
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-01-25 03:34:01 +0000

    coordinates ()

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -58,9 +58,9 @@ class Square:
             TypeError: position must be a tuple of 2 positive integers
 
         """
-        if not isinstance(value, tuple) or len(value) != 2 or
+        if not (isinstance(value, tuple) or len(value) != 2 or
                 not all(isinstance(num, int) for num in value) or
-                not all(num >= 0 for num in value):
+                not all(num >= 0 for num in value)):
             raise TypeError('position must be a tuple of 2 positive integers')
         self.__position = value
 
