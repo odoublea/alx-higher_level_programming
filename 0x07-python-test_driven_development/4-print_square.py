20c5682c55a91e0ebcf5e954b8abb4f5adea9dec
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-01-29 22:57:02 +0000

    damn the ';'

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+ This Module prints a square
+"""
+
+
+def print_square(size):
+    """
+    This function prints a square of hashes ``#``
+    Args:
+        size(int): The size of ``#`` to be printed
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    if isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+
+    for i in range(size):
+        for j in range(size):
+            print("{}".format('#'), end="")
+        print()
