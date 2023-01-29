3a09d7306a34eba052b891d91115d2559f7c0698
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-01-29 15:28:35 +0000

    learning is fun!

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -9,29 +9,30 @@
 
 def matrix_divided(matrix, div):
     """
-    This function takes a matrix (list of lists) and a divisor as input and returns the divided matrix.
-    Each element of the matrix is divided by the divisor and rounded to 2 decimal places.
+    This function takes a matrix (list of lists) and a divisor as input and
+    returns the divided matrix.
+    Each element of the matrix is divided by the divisor and rounded to 2
+    decimal places.
+
+    Args:
+        matrix (list of lists): The dividend
+        div: the divisor
     """
 
-    if type(matrix) != list or not all(isinstance(i, list) for i in matrix):
-        raise TypeError("matrix must be a list of lists")
-    # Checking if the input is a list of lists, if not raises a TypeError
+    if not isinstance(matrix, list) or not all(isinstance(i, list) for i in matrix)\
+            or not any(matrix):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
 
     if not all(isinstance(i, (int, float)) for row in matrix for i in row):
-        raise TypeError("matrix must contain only integers or floats")
-    # checking if the matrix contain only integers or floats, if not raises a TypeError
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
 
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
-    # Checking if all the rows have the same length, if not raises a TypeError
 
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
-    # Checking if div is a number, if not raises a TypeError
 
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    # Checking if div is zero, if it's the case raises a ZeroDivisionError
 
     return [[round(i / div, 2) for i in row] for row in matrix]
-    # returning the matrix where each element is divided by div and rounded to 2 decimal places
---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+    This module prints the name of a person
+    """
+
+
+def say_my_name(first_name, last_name=''):
+    """
+    Args:
+        first_name: string
+        last_name: string
+        """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
