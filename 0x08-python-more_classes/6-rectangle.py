6028ca68532ee0ad879d6ff13fd27ffe861d28b9
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-01-31 00:41:55 +0000

    How many instances

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -14,6 +14,7 @@ class Rectangle:
     number_of_instances = 0
     
     def __init__(self, width=0, height=0):
+        Rectangle.number_of_instances += 1
         self.width = width
         self.height = height
 
