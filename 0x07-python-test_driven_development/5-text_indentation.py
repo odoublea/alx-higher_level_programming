70fe4881174e7f38a8a73910c1410adb23eff61d
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-02-01 11:30:35 +0000

    No arguments taken

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
+
 """
     This module contains a function that split strings
     """
 
+
 def text_indentation(text):
-    """This function takes a string argument, text and print with 
-    2 new linws after each of these characters: ``.``, ``?`` and ``:``
+    """This function takes a string argument, text and print with
+    2 new linws after each of these characters: `.`, `?` and `:`
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
