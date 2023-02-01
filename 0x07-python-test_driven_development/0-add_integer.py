0a0b9ddea669bfc9f6c7480288f5ffe3f5ba0c64
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-02-01 10:11:28 +0000

    Code review

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -12,8 +12,8 @@ def add_integer(a, b=98):
     '''
     if None or not isinstance(a, (int, float)):
         raise TypeError('a must be an integer')
-    
+
     if None or not isinstance(b, (int, float)):
         raise TypeError('b must be an integer')
-    
+
     return int(a) + int(b)
