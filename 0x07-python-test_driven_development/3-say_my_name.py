c228a5713148b728445193babdaaaac4feab3714
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-02-01 11:19:57 +0000

    Spelling errors!

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -14,6 +14,8 @@ def say_my_name(first_name, last_name=''):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    if first_name == None or first_name == '':
-        raise TypeError("Missing first_name and last_name")
+    if not(first_name):
+        raise TypeError("Missing first_name")
+    if first_name is None and last_name is None:
+        raise TypeError("say_my_name() missing 1 required positional argument: 'first_name'")
     print("My name is {} {}".format(first_name, last_name))
