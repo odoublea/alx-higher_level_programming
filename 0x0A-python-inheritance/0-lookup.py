d2a9b12617292a3d516ff4fb77f2ce490364916b
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-02-06 21:35:00 +0000

    Lookup not Vlookup haha

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""
+    Attributes and methods of an object
+    """
+
+def lookup(obj):
+    """ This function returns attr and methods of a class object."""
+
+    return dir(obj)
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,6 @@
+# 0x0A. Python - Inheritance
+
+> - Inheritance
+> - Multiple inheritance
+> - Inheritance in Python
+> - Learn to Program 10 : Inheritance Magic Methods
