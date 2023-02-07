95ac231b43d5542779c865c6825305085ac9f07d
Oladega Abdulqudus <oladegakenny@gmail.com>
2023-02-07 02:22:54 +0100

    Only sub class of

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+    This function checks if an object is a subclass
+    of the specified class
+"""
+
+
+def inherits_from(obj, a_class):
+    """This functions checks if an object is a of or inherits from a
+    specified class.
+    """
+    if issubclass(type(obj), a_class):
+        return True
+    return False
