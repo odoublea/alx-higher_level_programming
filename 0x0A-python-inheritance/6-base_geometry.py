2cf3f92307f3662b091a5b129e7aea6f17d6509f
Oladega Abdulqudus <oladegakenny@gmail.com>
2023-02-07 02:43:01 +0100

    improve geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""
+    Geometry module
+"""
+
+
+class BaseGeometry:
+    """This is an empty geometry."""
+    def area(self):
+        raise Exception("area() is not implemented")
