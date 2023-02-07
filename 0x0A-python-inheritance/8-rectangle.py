27b01e5adc411ccd29b2cf0c454cfbed126dc5ed
Oladega Abdulqudus <oladegakenny@gmail.com>
2023-02-07 20:48:07 +0100

    rectangle height

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -10,16 +10,14 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """This is a rectangle classs with attr width and height.
-    It is a subclass of BaseGeometry
-    
+    It is a subclass of BaseGeometry 
     Args:
         width(int): must be int greater than 0 else error
         height(int): must be int greater than 0 else error
     """
 
     def __init__(self, width, height):
-        if self.integer_validator("width", width)\
-           and self.integer_validator("height", height):
+        if self.integer_validator("width", width):
             self.__width = width
+        if self.integer_validator("height", height):
             self.__height = height
-            
