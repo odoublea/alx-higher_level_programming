214dc13faf10481104478e5530788c7d7ce9182f
Oladega Abdulqudus <oladegakenny@gmail.com>
2023-02-07 23:38:04 +0100

    Square #1

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+    Geometry module
+"""
+
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """This is a rectangle classs with attr size.
+    It is a subclass of Rectangle
+
+    Args:
+        size(int): must be int greater than 0 else error
+    """
+
+    def __init__(self, size):
+        super().integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """Return the square area."""
+        return self.__size * self.__size
