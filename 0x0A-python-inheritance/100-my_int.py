ce1b0bd70184f626814c4474a6ab5c94d20d31bf
Oladega Abdulqudus <oladegakenny@gmail.com>
2023-02-08 00:33:29 +0100

    My integer

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""My integer module."""
+
+
+class MyInt(int):
+    """This is a rebel int class"""
+    def __init__(self, num):
+        if num == int:
+            return False
