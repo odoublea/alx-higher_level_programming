81097d399b1b69918598b206f210d006c47e06ec
Oladega Abdulqudus <oladegakenny@gmail.com>
2023-02-07 20:11:54 +0100

    Rectangle

---
+++ 0x0A-python-inheritance/8-base_geometry.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+    Geometry module
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """This is a rectangle classs with attr width and height.
+    It is a subclass of BaseGeometry
+    
+    Args:
+        width(int): must be int greater than 0 else error
+        height(int): must be int greater than 0 else error
+    """
+
+    def __init__(self, width, height):
+        if self.integer_validator("width", width)\
+           and self.integer_validator("height", height):
+            self.__width = width
+            self.__height = height
