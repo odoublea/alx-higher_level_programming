5a0ea73fa8e2e414cf4a0e0657bb2bf3f14a211e
Oladega Abdulqudus <oladegakenny@gmail.com>
2023-02-08 02:46:25 +0100

    write to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,6 +4,11 @@
 
 
 def write_file(filename="", text=""):
-    """This function writes to a text file."""
+    """This function writes to a text file.
+    Args:
+        filename: name of file to write to,
+            if none new one is created
+        text: the text to write
+        """
     with open(filename, 'w', encoding="utf-8") as f:
-        f.write(f"{text}\n")
+        return f.write(text)
