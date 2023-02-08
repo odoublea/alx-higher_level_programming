99da6dbc5e26e30d9fccd03b1f9296fd20c39e72
Oladega Abdulqudus <oladegakenny@gmail.com>
2023-02-08 14:52:47 +0100

    Student to JSON with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""The Student to JSON module."""
+
+
+class Student():
+    """This is a Class that defines Student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if (type(attrs) == list and all(type(i) == str for i in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
