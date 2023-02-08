b83bc8c94adf86a43e3746cb1330e1bc919a3de1
Oladega Abdulqudus <oladegakenny@gmail.com>
2023-02-09 00:00:14 +0100

    Pascal's Triangle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -4,11 +4,11 @@
 1. Create an empty list triangle (pascal) to store the rows of the triangle.
 2. Loop through each line from 1 to n+1
    (where n is the number of rows the triangle should have).
-3. Initialize a variable N to 1 for each line.
+3. Initialize a variable C to 1 to keep track of the current value.
 4. Create an empty list pascal to store the values of each line.
 5. Loop through each number in the line from 1 to line + 1.
-6. Append the value of N to the pascal.
-7. Update the value of N using the formula N = N * (line - i) / i.
+6. Append the value of C to the pascal.
+7. Update the value of C using the formula C = C * (line - i) / i.
 8. After the inner loop, append the pascal to the triangle list.
 9. Repeat the process for each line.
 10. Return the triangle list.
@@ -16,14 +16,15 @@
 
 
 def pascal_triangle(n):
-    triangle = []
     if n <= 0:
         return []
+
+    triangle = []
     for line in range(1, n+1):
-        N = 1
         pascal = []
+        C = 1
         for i in range(1, line+1):
-            pascal.append(N)
-            N = N * (line - i) // i
+            pascal.append(C)
+            C = C * (line - i) // i
         triangle.append(pascal)
     return triangle
