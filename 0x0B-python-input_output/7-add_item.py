a971cc808296adb97cb3fdd1f376f986eceb9844
Oladega Abdulqudus <oladegakenny@gmail.com>
2023-02-08 13:21:07 +0100

    Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,11 +9,11 @@ save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 if __name__ == "__main__":
-    new_list = []
     try:
         new_list = load_from_json_file("add_item.json")
     except FileNotFoundError:
         pass
+    new_list = []
 
     for i in argv[1:]:
         new_list.append(i)
