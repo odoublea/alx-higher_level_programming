db3a40131591ab67492c0e7c822a9dda7e488109
Oladega Abdulqudus <oladegakenny@gmail.com>
2023-02-14 22:27:37 +0100

    Json to string file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -31,7 +31,7 @@ class Base():
             list_dictionaries: list dictionary.
         """
         if list_dictionaries is None or list_dictionaries == []:
-            return "[]"
+            return json.dumps("[]")
         else:
             return json.dumps(list_dictionaries)
 
