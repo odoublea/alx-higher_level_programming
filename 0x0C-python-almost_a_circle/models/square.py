fb5137ed24dc8bce60beb6252a1835c4e6eaddbc
Oladega Abdulqudus <oladegakenny@gmail.com>
2023-02-14 22:23:19 +0100

    set Square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -16,27 +16,23 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """Super initializing attributes."""
         super().__init__(size, size, x, y, id)
-        self.size = size
 
     def __str__(self):
         """returns the square description."""
         desc = f"[{self.__class__.__name__}]"\
-               f" ({self.id}) {self.x}/{self.y} - {self.size}"
+               f" ({self.id}) {self.x}/{self.y} - {self.width}"
         return desc
 
     @property
     def size(self):
         """Get square size"""
-        return self.__size
+        return self.width
 
     @size.setter
     def size(self, value):
         """Set square size"""
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        if value <= 0:
-            raise ValueError("size must be > 0")
-        self.__size = value
+        self.width = value
+        self.height = value
 
     def update(self, *args, **kwargs):
         """Update the class Square"""
