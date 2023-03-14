406c1ffd4d7da9a53205a9e1a7993ba52a2f014c
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-03-14 15:25:29 -0700

    to UTF8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,3 +1,2 @@
 -- convert hbtn_0c_0 database to UTF8 on MySQL server
-ALTER DATABASE hbtn_0c_0
-CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
