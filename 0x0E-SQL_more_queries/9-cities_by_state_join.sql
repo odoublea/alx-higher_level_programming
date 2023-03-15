9a1dce548115ca9b0790d5f8cceeb5c32f87e096
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-03-15 10:55:01 -0700

    left join

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,5 @@
 -- List all cities contained in the database hbtn_0d_usa
 SELECT cities.id, cities.name, states.name
-FROM cities CROSS JOIN states
+FROM cities LEFT JOIN states
+ON states.id = cities.id
+ORDER BY cities.id
