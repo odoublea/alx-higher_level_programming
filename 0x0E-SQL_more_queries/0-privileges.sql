e2a27bbc142ef20b353ad496acc0e15af91c861a
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-03-15 08:28:36 -0700

    Root user

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- List all privileges of the MySQL users on the server
-SHOW GRANTS FOR user_0d_1;
-SHOW GRANTS FOR user_0d_2
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
