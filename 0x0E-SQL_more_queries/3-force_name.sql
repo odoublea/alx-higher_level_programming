2a6ae4889d439c0508eebfee70ca9d3d8284243a
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-03-15 09:06:20 -0700

    States table

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,4 +1,4 @@
--- Creare the table force_name on the MySQL server
+-- Create the table force_name on the MySQL server
 CREATE TABLE IF NOT EXISTS force_name (
 	id INT,
 	name VARCHAR(256) NOT NULL
--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,4 +1,4 @@
--- Creare the table unique_id on the MySQL server
+-- Create the table unique_id on the MySQL server
 CREATE TABLE IF NOT EXISTS unique_id (
 	id INT DEFAULT 1 UNIQUE,
 	name VARCHAR(256) NOT NULL
