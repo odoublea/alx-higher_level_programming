8382e2441ae92ae67ca4cd7dd3e91d169d66cd76
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-04-11 21:59:21 +0000

    standards

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,13 +3,13 @@
 const arg = parseInt(process.argv[2]);
 const printSquare = (size) => {
   const row = 'X'.repeat(size);
-    for (let i = 0; i < arg; i++) {
-      console.log(row);
-    }
+  for (let i = 0; i < arg; i++) {
+    console.log(row);
   }
+};
 
 if (Number.isInteger(arg)) {
-	printSquare(arg);
+  printSquare(arg);
 } else {
   console.log('Missing size');
 }
--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -5,6 +5,6 @@ const arg1 = parseInt(argv[2]);
 const arg2 = parseInt(argv[3]);
 
 const add = (arg1, arg2) => {
-	console.log(arg1 + arg2);
-}
+  console.log(arg1 + arg2);
+};
 add(arg1, arg2);
