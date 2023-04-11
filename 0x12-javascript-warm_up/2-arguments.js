6e145ff1533babbf1bc3f060dca8f210c5f5b382
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-04-11 14:07:37 +0000

    create a sentence

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 const { argv } = require('process');
 const arg = argv.length;
 if (arg <= 2) {
-	console.log('No argument');
+  console.log('No argument');
 } else if (arg === 3) {
-	console.log('Argument found');
+  console.log('Argument found');
 } else {
-	console.log('Arguments found');
-};
+  console.log('Arguments found');
+}
