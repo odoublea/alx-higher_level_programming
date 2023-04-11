d17b0fda28d94d647b18f7ed100b5c8ed4f08abc
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-04-11 21:18:38 +0000

    I love C

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const  items = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const items = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (const item of items) {
   console.log(item);
 }
--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,2 +1,14 @@
 #!/usr/bin/node
-const argv = require('process'); 
+
+const arg = parseInt(process.argv[2]);
+const printC = (value) => {
+  while (value > 0) {
+    console.log('C is fun');
+    value--;
+  }
+};
+if (Number.isInteger(arg)) {
+  printC(arg);
+} else {
+  console.log('Missing number of occurrences');
+}
