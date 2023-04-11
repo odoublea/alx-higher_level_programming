5badd40abd1845bfc058374d293de02e538d8a3e
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-04-11 16:58:30 +0000

    Not a number

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,7 +3,7 @@ const { argv } = require('process');
 const arg = parseInt(argv[2]);
 
 if (isNaN(arg)) {
-  console.log('Not a Number');
+  console.log('Not a number');
 } else {
   console.log(`My number: ${arg}`);
 }
