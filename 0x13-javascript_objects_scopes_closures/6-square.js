0883499edefb152de92c57f2537251d69321f92b
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-04-12 21:25:04 +0000

    Square Parent not Rectangle

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
+const Rectangle = require('./5-square');
 
 class Square extends Rectangle {
   constructor (size) {
-    super(size, size);
-   // this.size = size;
+    super(size);
   }
 
   charPrint (c) {
