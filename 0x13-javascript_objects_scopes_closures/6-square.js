19042fcfd9545efa617d067cbabcf0dc128e0177
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-04-12 21:22:33 +0000

    Parent attributes only

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,15 +4,15 @@ const Rectangle = require('./4-rectangle');
 class Square extends Rectangle {
   constructor (size) {
     super(size, size);
-    this.size = size;
+   // this.size = size;
   }
 
   charPrint (c) {
     if (c === undefined) {
       super.print();
     } else {
-      const printC = c.repeat(this.size);
-      for (let i = 0; i < this.size; i++) {
+      const printC = c.repeat(this.width);
+      for (let i = 0; i < this.height; i++) {
         console.log(printC);
       }
     }
