781f6db961121fe5145329ae79d48a817a5a8de2
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-04-15 12:16:31 +0000

    RTFM and get all states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    # Get command line arguments
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+
+    # Connect to the MySQL server
+    conn = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database, charset="utf8")
+
+    # Create a cursor object
+    cur = conn.cursor()
+
+    # Execute the SELECT query
+    cur.execute("SELECT * FROM states ORDER BY id ASC") # HERE I have to know SQL to grab all states in my database
+
+    # Fetch all rows from the query result
+    query_rows = cur.fetchall()
+
+    # Print each row
+    for row in query_rows:
+        print(row)
+
+    # Close the cursor and connection objects
+    cur.close()
+    conn.close()
