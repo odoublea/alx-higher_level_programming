6e926ee3bdd3fa0156608e7a5a352b0a8499c59f
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-04-16 00:00:39 +0100

    selectQuery

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -16,9 +16,9 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
 
     session = Session()
-    query = session.query(State).order_by(State.id).all()
+    selectQuery = session.query(State).order_by(State.id).all()
 
-    for state in query:
+    for state in selectQuery:
         print(f'{state.id}: {state.name}')
 
     session.close()
