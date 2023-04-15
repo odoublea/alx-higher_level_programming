1a2e8653320d02e898a9f93d868fbff623849695
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-04-15 14:55:08 +0000

    Cities by state

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    # Get command line arguments
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+
+    # Connect to the MySQL server
+    try:
+        conn = MySQLdb.connect(host="localhost", port=3306, user=username,
+                               passwd=password, db=database, charset="utf8")
+
+    except Exception:
+        print('Failed to connect to the database')
+        exit(0)
+
+    # Create a cursor object
+    cur = conn.cursor()
+
+    # Prepare the SELECT query
+    query = "SELECT cities.id, cities.name, states.name \
+             FROM cities \
+             JOIN states \
+             ON states.id = cities.state_id \
+             ORDER BY cities.id"
+
+    # Execute the query
+    cur.execute(query)
+
+    # Fetch all rows from the query result
+    query_rows = cur.fetchall()
+
+    # Print each row
+    for row in query_rows:
+        print(row)
+
+    # Close the cursor and connection objects
+    cur.close()
+    conn.close()
