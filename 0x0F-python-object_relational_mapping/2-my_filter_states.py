9f1a79b6f473657605b5e209bc03e4ecdc215c07
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-04-16 17:24:37 +0100

    level up

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,6 +6,7 @@
 import MySQLdb
 from sys import argv
 
+
 if __name__ == '__main__':
     '''This script takes in an argument and displays all values in
        the states table of hbtn_0e_0_usa where name matches the argument.
