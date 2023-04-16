c7b5fe1f8646617b7b836d40edd7c392202e84ee
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-04-16 21:13:44 +0000

    Delete state and pycode check

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-'''This script contains a script that deletes all State objects with a name containing the letter `a`
-from the database hbtn_0e_6_usa
+'''This script contains a script that deletes all State objects with a name
+containing the letter `a` from the database hbtn_0e_6_usa
 '''
 
 from sqlalchemy import create_engine
@@ -8,7 +8,7 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 from sys import argv
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     if len(argv) != 4:
         print(f"Usage: username password database")
         exit(1)
@@ -21,13 +21,12 @@ if __name__ = '__main__':
         f'mysql+mysqldb://{username}:{password}@localhost:3306/{database}'
     )
 
-    Base.metadata.create_all()
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
 
     session = Session()
-    state = session.query(State).filter(State.name.like('%a%')).all()
-
-    state.delete()
+    state = session.query(State).filter(State.name.like('%a%'))\
+        .delete(synchronize_session=False)
 
     session.commit()
     session.close()
