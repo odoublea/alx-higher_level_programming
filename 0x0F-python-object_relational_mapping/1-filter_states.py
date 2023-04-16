cedb5a0b2967c43f0559c2f1940cd6621fc59d86
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-04-16 17:20:50 +0100

    querySelect

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -27,11 +27,11 @@ if __name__ == '__main__':
     # Create a cursor object
     cur = conn.cursor()
 
-    #SELECT query
-    selectQuery = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id"
+    # SELECT query
+    # selectQuery = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id"
 
     # Execute the SELECT query
-    cur.execute(selectQuery)
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
 
     # Fetch all rows from the query result
     query_rows = cur.fetchall()
