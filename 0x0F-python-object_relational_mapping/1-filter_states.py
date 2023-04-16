49a5fc7e3492c792d4fc5cdd66d9431bdb341aa3
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-04-16 21:44:07 +0100

    one and two issue persists

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,9 +7,6 @@ from sys import argv
 
 
 if __name__ == '__main__':
-    '''This script lists all states with a name starting `N` from the database
-       hbtn_0e_0_usa
-    '''
     # Get command line arguments
     username = argv[1]
     password = argv[2]
