02d9abead5a7519ecfde82ca21ae292e62f281a9
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-04-16 21:32:42 +0100

    update a state using id

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+'''This module contains a script that changes the name of a State object
+from the database hbtn_0e_6_usa.
+'''
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sys import argv
+
+if __name__ == '__main__':
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+
+    engine = create_engine(
+        f'mysql+mysqldb://{username}:{password}@localhost:3306/{database}'
+    )
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    state = session.query(State).filter(State.id == 2)
+
+    if state is None:
+        print("State with id=2 not found")
+        exit(1)
+
+    state.name = "New Mexico"
+    session.commit()
+    session.close()
