3d20d2c7bc310ba2c486a2f924a93f86128e7c22
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-04-28 14:48:28 +0000

    get byte size only

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/bash
 # A Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-URL="$1"
-curl -s $URL | wc -c
+curl -s $1 | wc -c
