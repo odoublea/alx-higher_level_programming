54d301b521c45bcadf92b648dd98b9ec08f4e545
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-04-29 10:03:32 +0100

    What's my status? #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,8 +6,8 @@ if __name__ == "__main__":
     from urllib import request
 
     url = "https://alx-intranet.hbtn.io/status"
-    with request.urlopen(url) as response:
-        the_page = response.text
-        print("Body response:")
-        print("\t- type: {}".format(type(the_page)))
-        print("\t- content: {}".format(the_page))
+    response = request.get(url)
+    body = response.text
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
