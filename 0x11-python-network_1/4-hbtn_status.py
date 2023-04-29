53c7f405f5999bc43384caed51408a20f70d8dda
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-04-29 09:58:42 +0100

    what's my status? #1

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Python script that fetches https://alx-intranet.hbtn.io/status"""
+
+
+if __name__ == "__main__":
+    from urllib import request
+
+    url = "https://alx-intranet.hbtn.io/status"
+    with request.urlopen(url) as response:
+        the_page = response.read().decode('utf-8')
+        print("Body response:")
+        print("\t- type: {}".format(type(the_page)))
+        print("\t- content: {}".format(the_page))
