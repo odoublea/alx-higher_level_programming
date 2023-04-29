a75b6e7d9a23665046375a525ccba7f07c96c9c7
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-04-29 01:14:06 +0000

    cURL POST parameters

---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# A Bash script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response
+curl -s -X POST -F "email=test@gmail.com" -F "subjeck=I will always be here for PLD" $1
