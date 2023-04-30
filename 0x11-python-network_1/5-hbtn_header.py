2238e5b8804d9bf453234c42abda9269f024086a
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-04-30 06:47:06 +0000

    5-refactor

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -3,10 +3,9 @@
 displays the value of the variable X-Request-Id in the response header."""
 
 
+from sys import argv
+import requests
 if __name__ == "__main__":
-    import requests
-    import sys
-
-    url = sys.argv[1]
+    url = argv[1]
     response = requests.get(url)
     print(response.headers['X-Request-Id'])
