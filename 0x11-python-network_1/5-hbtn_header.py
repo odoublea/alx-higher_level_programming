cdde2f06511832de59c77981901a57be407b3b6b
Abdulqudus Oladega <oladegakenny@gmail.com>
2023-04-30 06:31:47 +0000

    5-pycode

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""Python script that takes in a URL, sends a request to the URL and displays
-the value of the variable X-Request-Id in the response header."""
+"""Python script that takes in a URL, sends a request to the URL and
+displays the value of the variable X-Request-Id in the response header."""
 
 
 if __name__ == "__main__":
@@ -9,5 +9,4 @@ if __name__ == "__main__":
 
     url = argv[1]
     response = requests.get(url)
-    header = response.headers["X-Request-Id"]
-    print(header)
+    print(response.headers["X-Request-Id"])
