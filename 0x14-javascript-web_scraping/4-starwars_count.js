834b27cd1e18446959fc4832697c22ef1d37d587
Abdulqudus Oladega <oladegakenny@gmail.com>
2024-01-05 17:39:32 +0100

    fix

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,7 +5,7 @@ request(url, (error, response, body) => {
   if (!error) {
     const results = JSON.parse(body).results;
     console.log(results.reduce((count, movie) => {
-      return movie.characters.find((character) => character.endwith('/18/'))
+      return movie.characters.find((character) => character.endsWith('/18/'))
         ? count + 1
         : count;
     }, 0));
