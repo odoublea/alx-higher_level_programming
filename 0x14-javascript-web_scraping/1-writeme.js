e80e94901660acfa60a93c395dc818b7cc07e926
Abdulqudus Oladega <oladegakenny@gmail.com>
2024-01-05 15:42:33 +0100

    write me

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,9 +4,5 @@ const fs = require('fs'); const filePath = process.argv[2];
 const content = process.argv[3];
 
 fs.writeFile(filePath, content, 'utf8', (err) => {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log(`The file ${filePath} was successfully saved.`);
-  }
+  if (err) { console.error(err); }
 });
